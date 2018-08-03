c9d99922fe00117932a9877a494e2f9213c9d8be
rhcmoore <40081284+rhcmoore@users.noreply.github.com>
2018-08-03 15:38:23 -0700

    Updating for new instances format

--- tiles.py
+++ tiles.py
@@ -1,7 +1,14 @@
-# import the pygame module, so you can use it
-import pygame as pyg
 import os
-default = 1            
+import random
+class Tile:
+
+
+    def __init__(self, name, score, image, modifier):
+        self.name = name
+        self.score = score
+        self.image = image
+        self.modifier = modifier
+
 tileList = {"A":'tiles/A.png',
             "B":'tiles/B.png',
             "C":'tiles/C.png',
@@ -27,60 +34,15 @@ tileList = {"A":'tiles/A.png',
             "W":'tiles/W.png',
             "X":'tiles/X.png',
             "Y":'tiles/Y.png',
-            "Z":'tiles/Z.png',
-}
-# create class
-class Tile:
-    tileScore ={"A":default, 
-            "B":3 ,
-            "C":3 ,
-            "D":default ,
-            "E":default ,
-            "F":4 ,
-            "G":2 ,
-            "H":4 , 
-            "I":default , 
-            "J":8 ,
-            "K":5 ,
-            "L":default , 
-            "M":3 , 
-            "N":default ,
-            "O":default ,
-            "P":3 ,
-            "Q":10 , 
-            "R":default ,
-            "S":default ,
-            "T":default ,
-            "U":default ,
-            "V":4 ,
-            "W":4 ,
-            "X":8 ,
-            "Y":4 ,
-            "Z":10 }
+            "Z":'tiles/Z.png'}
+
+blank_tile = Tile('blank', 0, 'blank_image', 'None')
+
+A_tile1 = Tile('A', 1, pyg.transform.scale(pyg.image.load(os.path.join(tileList['A'])), (30, 30)), 'None')
+A_tile2 = Tile('A', 1, pyg.transform.scale(pyg.image.load(os.path.join(tileList['A'])), (30, 30)), 'None')
+A_tile3 = Tile('A', 1, pyg.transform.scale(pyg.image.load(os.path.join(tileList['A'])), (30, 30)), 'None')
+B_tile = Tile('B', 2, pyg.transform.scale(pyg.image.load(os.path.join(tileList['B'])), (30, 30)), 'None')
+C_tile = Tile('C', 3, pyg.transform.scale(pyg.image.load(os.path.join(tileList['C'])), (30, 30)), 'None')
 
 
-    # tile = pyg.image.load(os.path.join('tiles/F.png'))
-    # declear variables
-    # F_tile = pyg.image.load(os.path.join('tiles/F.png'))
-
-    def __init__(self):
-        pass
-        # self.tileName = tileName
-        # self.x = x
-        # self.y = y
-        # self.tile = tile\
-
-    def loadImage(self,tileName):
-        # if tileName == "F":
-        tile = pyg.image.load(os.path.join(tileList[tileName]))
-        # elif tileName == "G":
-            # tile = pyg.image.load(os.path.join('tiles/G.png'))
-        return tile
-
-    def setSize(self,x,y,tile):
-        newTile = pyg.transform.scale(tile, (x, y))
-        return newTile
-        # self.F_tile = F_tile 
-        
-
-    # def lordImage()
+tileBank = [A_tile1, A_tile2, A_tile3, B_tile, C_tile]

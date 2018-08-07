f0260a3d7620d053daabe2d21f51278c56dc7c33
maxxie114 <maxxie114@gmail.com>
2018-08-07 02:02:35 -0700

    Delete NEWplayer.py

--- NEWplayer.py
+++
@@ -1,9 +0,0 @@
-class Player:
-
-    def __init__(self, name):
-        self.name = name
-        self.tiles = []
-        self.score = 0
-
-Player1 = Player('Player 1')
-Player2 = Player('Player 2')

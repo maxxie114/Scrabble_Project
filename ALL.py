419cfbd6fe0a0b8e030fc2174b767b59c03395c6
rhcmoore <40081284+rhcmoore@users.noreply.github.com>
2018-08-08 13:43:12 -0700

    Added tileScore dictionary

--- ALL.py
+++ ALL.py
@@ -33,6 +33,34 @@ tileList = {"A":'tiles/A.png',
             "NA":'tiles/empty.png',
             'white':'tiles/white.png'}
 
+# Dictionary with keys:letters, and values:scores
+tileScore ={"A":1, 
+            "B":3 ,
+            "C":3 ,
+            "D":1 ,
+            "E":1 ,
+            "F":4 ,
+            "G":2 ,
+            "H":4 , 
+            "I":1 , 
+            "J":8 ,
+            "K":5 ,
+            "L":1 , 
+            "M":3 , 
+            "N":1 ,
+            "O":1 ,
+            "P":3 ,
+            "Q":10 , 
+            "R":1 ,
+            "S":1 ,
+            "T":1 ,
+            "U":1 ,
+            "V":4 ,
+            "W":4 ,
+            "X":8 ,
+            "Y":4 ,
+            "Z":10 }
+
 blue_color = (0, 0, 255)
 green_color = (0, 255, 0)
 red_color = (255, 0, 0)
